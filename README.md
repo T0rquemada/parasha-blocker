a8f243455a0c50cfaea06dc983f618b048d27ea2
T0rquemada <viktor.debian@gmail.com>
2025-10-08 15:10:10 +0300

    v 1.0

---
+++ README.md
@@ -0,0 +1,18 @@
+# Parasha Blocker
+
+A Google Chrome extension that hides all russian and pro-russian websites in search results.
+It also hides news and all images from the corresponding tab for blocked sites.
+
+Works in Google Chrome with the following search engines:
+1. DuckDuckGo
+
+## How to Install
+1. Install .zip archive from Releases
+2. Open [chrome://extensions/](chrome://extensions/)
+3. In the top-right corner, enable **Developer mode**
+4. In the top-left corner, click **Load unpacked**
+5. Select the folder containing the unpacked extension
+6. Done
+
+## ToDo
+- [ ] Add support for Google Search Engine
---
+++ content.js
@@ -0,0 +1,58 @@
+// List of domains to block. If a link contains any of these domains, it will be hidden
+const parashaList = [".ru", ".by", ".kz", ".kg", "yandex", "vk.com", "vk.me"];
+
+// Removes an element from the page
+function removeElement(el)
+{
+    el.remove();
+}
+
+// Function to process search results for the DuckDuckGo search engine
+function duckDuckGo()
+{
+    // "All" tab
+    document.querySelectorAll('a[data-testid="result-title-a"]').forEach(link => 
+    {
+        if(parashaList.some(domain => link.href.includes(domain))) 
+        {
+            removeElement(link.closest("article"));
+        }
+    });
+
+    // "Images" tab
+    document.querySelectorAll(".nsogf_Hpj9UUxfhcwQd5").forEach(imgBlock =>
+    {
+        const link = imgBlock.querySelector(".iHufrGzRLnW5Wh3koaLG");
+
+        if(link && parashaList.some(domain => link.textContent.includes(domain))) 
+        {
+            const imageListItem = imgBlock.parentNode;
+            removeElement(imageListItem);
+        }
+    });
+
+    // "News" tab
+    document.querySelectorAll(".O9Ipab51rBntYb0pwOQn").forEach(newsBlock => {
+        const linkDiv = newsBlock.querySelector(".hsgD5pJVzSAt58DaSA9w");
+        if(linkDiv === null)
+        {
+            return;
+        }
+
+        const link  = linkDiv.querySelector("span").textContent;
+        if(parashaList.some(domain => link.includes(domain)))
+        {
+            removeElement(newsBlock);
+        }
+    });
+}
+
+function main() 
+{
+    duckDuckGo();
+}
+
+main();
+
+const observer = new MutationObserver(main);
+observer.observe(document.body, { childList: true, subtree: true });
